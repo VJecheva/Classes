7dc2ac5bc81dce245ff1e785102545aa532626b3
Веселина Жечева <vessi@bfu.bg>
2022-04-04 13:30:05 +0300

    class

---
+++ TestClasses/TestClasses/Rectangle.h
@@ -0,0 +1,14 @@
+#pragma once
+class Rectangle {
+private:
+	int a, b;
+public: 
+	Rectangle(int = 1, int = 1);
+	void setA(int);
+	void setB(int);
+	int getA()const;
+	int getB()const;
+	void print()const;
+	int area()const;
+	int perimeter()const;
+};
